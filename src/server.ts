5ecc2474150fb6ea2f56775c8e258ff59c1308c5
orkarfabianthewise <orkarmelch@gmail.com>
2025-12-04 22:42:37 +0100

    port fixed

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ import usersRoute from './usersRoute.js';
 dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
